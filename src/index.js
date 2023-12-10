f265f60817233123bce479a46f511bd4983e8f0d
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2023-12-10 11:43:37 +0100

    task-01 done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import "./styles/normalize.css";
 import "./styles/index.css";
+import {getAllProducts} from "./requests/products"
+import { renderCarts } from "./js/renderCarts"
+
+getAllProducts()
+
+renderCarts()
+
+// console.log(getAllProducts())
